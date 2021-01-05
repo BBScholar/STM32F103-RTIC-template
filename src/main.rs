27dfe27fe16f9a76f1c7457963b7f97623a30cdd
Ben Scholar <bbs27@case.edu>
2021-01-04 21:28:09 -0800

    updated template with defmt support

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![feature(lang_items)]
+#![feature(lang_items, panic_info_message, fmt_as_str)]
 #![no_std]
 #![no_main]
 #![allow(dead_code)]
@@ -17,7 +17,12 @@ use rtic::cyccnt::{Instant, U32Ext as _};
 use stm32f1xx_hal::gpio;
 use stm32f1xx_hal::pac;
 
-use core::convert::{Into};
+use core::convert::Into;
+
+use defmt;
+
+/// this makes sure that the rtt logger is linked into the binary
+use defmt_rtt as _;
 
 const HSE_CLOCK_MHZ: u32 = 8;
 const SYS_CLOCK_MHZ: u32 = 72;
@@ -25,22 +30,30 @@ const SYS_CLOCK_MHZ: u32 = 72;
 const HSE_CLOCK_HZ: u32 = HSE_CLOCK_MHZ * 1_000_000;
 const SYS_CLOCK_HZ: u32 = SYS_CLOCK_MHZ * 1_000_000;
 
+#[defmt::timestamp]
+fn timestamp() -> u64 {
+    DWT::get_cycle_count() as u64
+}
+
 #[app(device=stm32f1::stm32f103, peripherals = true, monotonic=rtic::cyccnt::CYCCNT)]
 const APP: () = {
-
     struct Resources {
+        #[init(true)]
+        power_ok: bool,
     }
-    
+
     #[init(schedule=[])]
     fn init(cx: init::Context) {
         let mut peripherals = cx.core;
         let device: stm32f1xx_hal::stm32::Peripherals = cx.device;
-        
+
         let mut flash = device.FLASH.constrain();
         let mut rcc = device.RCC.constrain();
         let mut afio = device.AFIO.constrain(&mut rcc.apb2);
-        
-        let clocks = rcc.cfgr.use_hse(HSE_CLOCK_MHZ.mhz())
+
+        let clocks = rcc
+            .cfgr
+            .use_hse(HSE_CLOCK_MHZ.mhz())
             .sysclk(SYS_CLOCK_MHZ.mhz())
             .hclk(SYS_CLOCK_MHZ.mhz())
             .pclk1((SYS_CLOCK_MHZ / 2).mhz())
@@ -49,18 +62,18 @@ const APP: () = {
 
         let mut gpioa = device.GPIOA.split(&mut rcc.apb2);
         let mut gpiob = device.GPIOB.split(&mut rcc.apb2);
-        
+
         peripherals.DCB.enable_trace();
         DWT::unlock();
         peripherals.DWT.enable_cycle_counter();
-    
+
         // let now = cx.start;
     }
 
     #[idle()]
     fn idle(_cx: idle::Context) -> ! {
         loop {
-            cortex_m::asm::wfi();
+            core::sync::atomic::spin_loop_hint();
         }
     }
 
@@ -71,11 +84,26 @@ const APP: () = {
         fn SPI2();
         fn SPI3();
     }
-
 };
 
 #[panic_handler]
-fn my_panic(_: &core::panic::PanicInfo) -> ! {
+#[inline(never)]
+fn my_panic(info: &core::panic::PanicInfo) -> ! {
+    use core::fmt::Debug;
+
+    use defmt::{consts, Debug2Format};
+
+    defmt::error!(
+        "Panic: \"{:?}\" \nin file {:str} at line {:u32}",
+        Debug2Format::<U1024>(info.message().unwrap()),
+        info.location().unwrap().file(),
+        info.location().unwrap().line()
+    );
+    loop {}
+}
+
+#[defmt::panic_handler]
+fn defmt_panic() -> {
     loop {}
 }
 
